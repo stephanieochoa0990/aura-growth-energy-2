58e05ee07d272c09519b87e0187117bf24bf19f6
Stephanie Ochoa <stephanieochoa0990@gmail.com>
2025-11-24 23:29:22 -0600

    Add vercel SPA rewrite configuration

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import AdminDashboard from '@/components/admin/AdminDashboard';
+import AdminDashboard from "@/components/admin/AdminDashboard";
 
 export default function AdminPage() {
   return <AdminDashboard />;
